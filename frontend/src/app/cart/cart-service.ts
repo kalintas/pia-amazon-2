c7f9803aaa62ff2328914ae90cbefb19ac3ce6a4
kalintas <keremkalinntas@gmail.com>
2025-08-27 13:20:08 +0300

    Start fixing the cart component

--- frontend/src/app/cart/cart-service.ts
+++ frontend/src/app/cart/cart-service.ts
@@ -5,41 +5,57 @@ export interface CartItem extends Product {
   quantity: number,
 }
 
+interface Cart {
+  [id: string]: CartItem;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  items: WritableSignal<Array<CartItem>> = signal([]);
-  total_price = computed(() => {
+  items: WritableSignal<Cart> = signal({});
+
+  totalPrice = computed(() => {
     let result = 0;
-    this.items().forEach((item) => {
-        result += item.price;
+    Object.values(this.items()).forEach((item) => {
+      result += item.price * item.quantity;
     })
     return result;
-  })
+  });
 
   addToCart(product: Product) {
     this.items.update(list => {
-      const existingItem = list.find(item => item.id === product.id);
-
-      if (existingItem) {
-        return list.map(item =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
+      if (list[product.id]) {
+        list[product.id].quantity += 1;
       } else {
-        return [...list, { ...product, quantity: 1 }];
+        list[product.id] = { ...product, quantity: 1 };
       }
+      return {...list};
     });
   }
   clearCart() {
-    this.items.set([]);
+    this.items.set({});
   }
   removeItem(item: CartItem) {
-
+    this.items.update((list) => {
+      delete list[item.id];
+      return {...list};
+    })
   }
   checkout() {
+  }
+
+  getItems(): Array<CartItem> {
+    return Object.values(this.items())
+  }
+
+  changeQuantity(item: CartItem, change: number) {
+    this.items.update((list) => {
+      list[item.id].quantity += change;
+      if (list[item.id].quantity <= 0) {
+        delete list[item.id];
+      }
+      return {...list};
+    });
   }
 }
