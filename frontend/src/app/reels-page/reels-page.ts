a63da542b6b55d93efe778d3bb28bdfb7847abe1
Can Özer <canozer.pirireis@gmail.com>
2025-08-28 14:38:50 +0300

    New features added to reels page
    Users now cannot select product image, name, description and price and also like, dislike and comment buttons. Now if you're not signed in if you click on of those buttons there is a new popup that tolds you must sign in.

--- frontend/src/app/reels-page/reels-page.ts
+++ frontend/src/app/reels-page/reels-page.ts
@@ -1,113 +1,135 @@
 import { NgStyle } from '@angular/common';
-import { Component, effect, HostListener, inject, Input, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  effect,
+  HostListener,
+  inject,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../services/api-service';
 import { Product } from '../interfaces/product';
 import { Reel } from '../interfaces/reel';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-reels-page',
-    imports: [NgStyle],
-    templateUrl: './reels-page.html',
-    styleUrl: './reels-page.css',
+  selector: 'app-reels-page',
+  imports: [NgStyle, Header, Footer, NgStyle, ReactiveFormsModule],
+  templateUrl: './reels-page.html',
+  styleUrl: './reels-page.css',
 })
 export class ReelsPage {
-    apiService: ApiService = inject(ApiService);
-    commentsEnabled = signal(false);
+  apiService: ApiService = inject(ApiService);
+  commentsEnabled = signal(false);
+  signInEnabled = signal(false);
 
-    reelIds: Array<string> = [];
-    currentReelIndex: WritableSignal<number> = signal(0);
+  reelIds: Array<string> = [];
+  currentReelIndex: WritableSignal<number> = signal(0);
 
-    reel: WritableSignal<Reel | null> = signal(null);
-    product: WritableSignal<Product | null> = signal(null);
+  reel: WritableSignal<Reel | null> = signal(null);
+  product: WritableSignal<Product | null> = signal(null);
 
-    canSwipe = true;
-    Array = Array;
+  canSwipe = true;
+  Array = Array;
 
-    constructor(private router: Router) {
-        effect(() => {
-            const index = this.currentReelIndex();
-            this.fetchReel(index);
+  constructor(private router: Router) {
+    effect(() => {
+      const index = this.currentReelIndex();
+      this.fetchReel(index);
+    });
+  }
+
+  fetchReel(index: number) {
+    this.canSwipe = false;
+    let id = undefined;
+    if (index < this.reelIds.length && this.reelIds[index]) {
+      id = this.reelIds[index];
+    }
+
+    this.apiService.getReel(id).subscribe((reel: Reel) => {
+      if (!reel) {
+        return;
+      }
+      console.log(reel);
+      this.apiService
+        .getProduct(reel.productId)
+        .subscribe((product: Product) => {
+          this.product.set(product);
+          console.log(product);
         });
+      this.reel.set(reel);
+      if (index >= this.reelIds.length) {
+        console.log(reel.id);
+        this.reelIds.push(reel.id);
+      }
+      this.canSwipe = true;
+    });
+  }
+
+  increaseLike() {}
+
+  increaseDislike() {}
+
+  increaseComment() {}
+
+  backToHomePage() {
+    this.router.navigate(['home']);
+  }
+
+  goToProductPage() {
+    const product = this.product();
+    if (product) {
+      this.router.navigate(['product', product.id]);
+    }
+  }
+
+  goToSignInPage() {
+    this.router.navigate(['auth']);
+  }
+
+  // Either a negative or a positive number
+  lastSwipe = 0;
+
+  @HostListener('wheel', ['$event'])
+  onSwipe(event: WheelEvent) {
+    event.preventDefault();
+
+    const THRESHOLD = 10;
+
+    if (!this.canSwipe) {
+      return;
     }
 
-    fetchReel(index: number) {
-        this.canSwipe = false;
-        let id = undefined;
-        if (index < this.reelIds.length && this.reelIds[index]) {
-            id = this.reelIds[index]
-        }
-
-        this.apiService.getReel(id).subscribe((reel: Reel) => {
-            if (!reel) {
-                return;
-            }
-            console.log(reel)
-            this.apiService
-                .getProduct(reel.productId)
-                .subscribe((product: Product) => {
-                    this.product.set(product);
-                    console.log(product);;
-                });
-            this.reel.set(reel);
-            if (index >= this.reelIds.length) {
-                console.log(reel.id);
-                this.reelIds.push(reel.id);
-            }
-            this.canSwipe = true;
-        });
+    if (
+      Math.sign(this.lastSwipe) === Math.sign(event.deltaY) &&
+      Math.abs(event.deltaY) > THRESHOLD
+    ) {
+      return;
     }
 
-
-    increaseLike() {
+    if (event.deltaY > THRESHOLD) {
+      // Get next reel.
+      console.log('Next');
+      this.currentReelIndex.update((index) => index + 1);
+      this.lastSwipe = event.deltaY;
+    } else if (event.deltaY < -THRESHOLD) {
+      // Get prev reel.
+      console.log('Prev');
+      this.currentReelIndex.update((index) => Math.max(0, index - 1));
+      this.lastSwipe = event.deltaY;
+    } else {
+      this.lastSwipe = 0;
     }
+  }
 
-    increaseDislike() {
+  openSignInPopUp() {
+    if (this.apiService.user() == null) {
+      this.signInEnabled = signal(true);
+    } else {
+      this.signInEnabled = signal(false);
     }
-
-    increaseComment() {
-    }
-
-    backToHomePage() {
-        this.router.navigate(['home']);
-    }
-
-    goToProductPage() {
-        const product = this.product();
-        if (product) {
-            this.router.navigate(['product', product.id])
-        }
-    }
-
-    // Either a negative or a positive number
-    lastSwipe = 0;
-
-    @HostListener('wheel', ['$event'])
-    onSwipe(event: WheelEvent) {
-        event.preventDefault();
-
-        const THRESHOLD = 10;
-
-        if (!this.canSwipe) {
-            return;
-        }
-
-        if (Math.sign(this.lastSwipe) === Math.sign(event.deltaY) && Math.abs(event.deltaY) > THRESHOLD) {
-            return;
-        }
-
-        if (event.deltaY > THRESHOLD) {
-            // Get next reel.
-            console.log("Next")
-            this.currentReelIndex.update((index) => index + 1);
-            this.lastSwipe = event.deltaY;
-        } else if (event.deltaY < -THRESHOLD) {
-            // Get prev reel.
-            console.log("Prev")
-            this.currentReelIndex.update((index) => Math.max(0, index - 1));
-            this.lastSwipe = event.deltaY;
-        } else {
-            this.lastSwipe = 0;
-        }
-    }
+  }
 }
